b2b07d24fe556d7ec32452852aa8269980994c61
AnliClark <le.clark9999@Gmail.com>
2025-04-10 00:04:02 +0800

    增加了组件的基类，以及与或非的逻辑，电平的规定

---
+++ src/logic/components/OrGate.js
@@ -0,0 +1,32 @@
+import { BaseComponent } from "../BaseComponent";
+import { SignalState } from "../SignalState";
+
+export class OrGate extends BaseComponent {
+    compute() {
+        for (const value of this.inputs) {
+            if (value === SignalState.DISCONNECTED) {
+                this.outputs = [SignalState.DISCONNECTED];
+                return this.outputs;
+            }
+            if (value === SignalState.HIGH) {
+                this.outputs = [SignalState.HIGH];
+                return this.outputs;
+            }
+        }
+        this.outputs = [SignalState.LOW];
+        return this.outputs;
+    }
+
+    changeInput(index, v) {
+        this.inputs[index] = v;
+        if (v === SignalState.DISCONNECTED) {
+            this.outputs = [SignalState.DISCONNECTED];
+            return this.outputs;
+        }
+        if (v === SignalState.HIGH) {
+            this.outputs = [SignalState.HIGH];
+            return this.outputs;
+        }
+        return this.compute();
+    }
+}
