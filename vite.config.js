3939f4061e9de25e62130e6f5019d3760f5c89d8
pathwoven <le.clark9999@Gmail.com>
2025-05-06 11:21:30 +0800

    修改了vite和ts的配置，增加了全局状态管理文件

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)) // 使用 @ 代替 src 目录
     },
     extensions: ['.js', '.vue', '.json'] // 导入时可忽略的文件扩展名
-  }
+  },
+  assetsInclude: ['./src/assets'],
+  build: {
+    outDir: 'dist', // 确保与 tsconfig.json 的 outDir 一致
+  },
 })
