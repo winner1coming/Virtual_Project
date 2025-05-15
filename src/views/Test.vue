ad7aa5b07c991c3c0ba7fc2bf71445247a2a74b8
pathwoven <le.clark9999@Gmail.com>
2025-05-15 22:33:57 +0800

    新增了异或门、与非门、或非门、时钟的电路逻辑；发现电路无法动态更新的bug

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,5 @@
-<script setup>
-import { ref } from 'vue'
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
 
 import { AndGate } from "@/logic/components/AndGate.js";
 import { OrGate } from "@/logic/components/OrGate.js";
@@ -7,7 +7,7 @@ import { NotGate } from "@/logic/components/NotGate.js";
 import {CircuitLogic} from "@/logic/CircuitLogic.js";
 import {useCircuitStore} from "@/store/CircuitStore"
 
-function testConnect(){
+
     let circuitLogic = CircuitLogic.getInstance();
     const circuitStore = useCircuitStore();
     // 创建逻辑门实例
@@ -15,6 +15,8 @@ function testConnect(){
     circuitStore.addComponent("Or", [0,0]);
     circuitStore.addComponent("Not", [0,0]);
     circuitStore.addComponent("And", [0,0]);
+    circuitStore.addComponent("Clock", [0,0]);
+
     let and1 = circuitStore.getComponent(0);
     let or1 = circuitStore.getComponent(1);
     let not1 = circuitStore.getComponent(2);
@@ -46,22 +48,64 @@ function testConnect(){
     
     circuitLogic.connect(3,2,0,1);
 
+    let clock = circuitStore.getComponent(4);
+    circuitLogic.connect(4, 0, 0, 0);
+
     console.log("andGate一个输入为0，andGate输出给orGate，orGate输出给Not")
     console.log("AndGate instance:", and1);
     console.log("OrGate instance:", or1);
     console.log("NotGate instance:", not1);  
 
-}
 
-function handleClick() {
-    testConnect();
-}
+const outputs = ref({
+  and2: and2.getOutputs()[0],
+  clock: clock.getOutputs()[0],
+
+  and1: and1.getOutputs()[0],
+  
+  or1: or1.getOutputs()[0],
+  not1: not1.getOutputs()[0],
+  
+})
+let timer: any = null;
+onMounted(() => {
+  timer = setInterval(() => {
+    outputs.value.and1 = and1.getOutputs()[0];
+    outputs.value.and2 = and2.getOutputs()[0];
+    outputs.value.or1 = or1.getOutputs()[0];
+    outputs.value.not1 = not1.getOutputs()[0];
+    outputs.value.clock = clock.getOutputs()[0];
+
+    console.log(and1.getOutputs()[0]);
+  }, 100); // 100ms刷新一次
+});
+onUnmounted(() => {
+  clearInterval(timer);
+});
+// function handleClick() {
+//     testConnect();
+// }
+
 </script>
 
 <template>
   <div>
     <h1>测试页面</h1>
-    <button @click="handleClick">点击我</button>
+     {{ "与门2   " + outputs.and2 }}
+    <br>
+    {{ "时钟" + outputs.clock }}
+    <br>
+    {{ "与门：" + outputs.and1 }}
+    <br>
+    {{ "或门" + outputs.or1 }}
+    <br>
+    {{ "非门" + outputs.not1 }}
+
+    <br>
+
+   
+
+
   </div>
 </template>
 
