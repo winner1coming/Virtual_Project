8007a3bbbed2d1ab9364952f97f64ecb2af83d49
pathwoven <le.clark9999@Gmail.com>
2025-07-04 21:42:27 +0800

    完善其他元件的输入取反，修复原先与非门和或非门的输出bug

--- src/logic/components/OrGate.ts
+++ src/logic/components/OrGate.ts
@@ -21,32 +21,36 @@ export class OrGate extends BaseComponent {
     //       ];
     //     });
     // }
-    compute() {
-        let hasConnected = false;
-        for(const value of this.inputs){
-            if(value === -2){
-                //this.outputs[0] = -2;
-                this.outputs.splice(0, this.outputs.length, -2); 
-                return this.outputs;
-            }
-            if(value !== -1){
-                if(!hasConnected){
-                    hasConnected = true;
-                    // this.outputs[0] = value;
-                    this.outputs.splice(0, 1, value); 
-                } else{
-                    // this.outputs[0] = this.outputs[0] | value;
-                    this.outputs.splice(0, 1, this.outputs[0] | value); 
-                }
-            }
-            
-        }
-        if(!hasConnected) {
-            // this.outputs[0] = -1;
-            this.outputs.splice(0, this.outputs.length, -1); 
-        }
-        return this.outputs;
-    }
+    compute(){   // 返回输出(int)
+		let hasConnected = false;
+		const mask = (1 << this.bitWidth) - 1;
+		for (let i = 0; i < this.inputs.length; i++) {
+			let value = this.inputs[i];
+
+			// 如果输入引脚被设置为取反，则取反值
+			if (this.inputInverted[i]) {
+				value = value === -1 || value === -2 ? value : ~value & mask; // 保持 -1 和 -2 不变，其他值取反
+			}
+
+			if (value === -2) {
+				this.outputs.splice(0, this.outputs.length, -2);
+				return this.outputs;
+			}
+
+			if (value !== -1) {
+				if (!hasConnected) {
+				hasConnected = true;
+				this.outputs.splice(0, 1, value); 
+				} else {
+				this.outputs.splice(0, 1, this.outputs[0] | value); 
+				}
+			}
+		}
+		if(!hasConnected) {
+			this.outputs.splice(0, this.outputs.length, -1); // 如果没有连接任何输入，则输出-1
+		}
+		return this.outputs;
+	}
 
     changeInput(idx: number, v: number): number[]{
         // this.inputs[idx] = v;
