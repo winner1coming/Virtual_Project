18a921dfaade851266549e2141473d90813ed72c
pathwoven <le.clark9999@Gmail.com>
2025-07-06 16:08:33 +0800

    将子组件的计算改为真值表

--- src/logic/ProjectData.ts
+++ src/logic/ProjectData.ts
@@ -3,6 +3,7 @@ import type { Conn } from './ConnectionManager';
 
 export interface ProjectData {
   projectId: number;
+  projectUUID: string; // 项目唯一标识符
   name: string;
   componentsId: number[]; // 元件的id数组
   inputPins: number[];    // 输入引脚的id的数组
--- src/store/ProjectStore.ts
+++ src/store/ProjectStore.ts
@@ -13,6 +13,7 @@ export const useProjectStore = defineStore('project', () => {
   // 初始化一个默认项目
   const defaultProject: ProjectData = {
     projectId: currentProjectId.value++,
+    projectUUID: crypto.randomUUID(), 
     name: "新项目",
     componentsId: [],
     inputPins: [],
@@ -25,6 +26,7 @@ export const useProjectStore = defineStore('project', () => {
   function createProject(name: string): ProjectData {
     const project: ProjectData = {
       projectId: currentProjectId.value++,
+      projectUUID: crypto.randomUUID(), // 生成一个新的UUID
       name,
       componentsId: [],
       inputPins: [],
